d6b019e59b85290b2a0ef9b41dad774267537554
red-ait <redouanmkdem@gmail.com>
2021-01-20 21:41:41 +0100

    create project structure

---
+++ Backend/src/main/java/fr/isima/etudecaswebmobile/EtudeCasWebMobileApplication.java
@@ -0,0 +1,13 @@
+package fr.isima.etudecaswebmobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtudeCasWebMobileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtudeCasWebMobileApplication.class, args);
+    }
+
+}
