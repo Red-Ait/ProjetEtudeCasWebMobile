ee1b668cf3057e6cb90b510636754af30b8ec130
SIMOACR <medachkour@gmail.com>
2021-02-07 00:14:33 +0100

    collection Form

--- frontend2/src/app/app.module.ts
+++ frontend2/src/app/app.module.ts
@@ -7,11 +7,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { StoreModule } from '@ngrx/store';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, StoreModule.forRoot({}, {})],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, StoreModule.forRoot({}, {}), BrowserAnimationsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- frontend2/src/app/v0/location/location-routing.module.ts
+++ frontend2/src/app/v0/location/location-routing.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {TagsComponent} from './containers/tags/tags.component';
+import {CollectionComponent} from "./containers/collection/collection.component";
 
 const routes: Routes = [
-  { path: 'tags', component: TagsComponent }
+  { path: 'tags', component: TagsComponent },
+  { path: 'collections', component: CollectionComponent }
 ];
 
 @NgModule({
