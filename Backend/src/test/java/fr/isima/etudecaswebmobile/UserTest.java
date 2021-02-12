59a5bc7286e853944df1b2f6e2fb4a5e86e01045
HUODIL <lidouhm@gmail.com>
2021-02-12 20:36:40 +0100

    Implement User Test

--- Backend/src/test/java/fr/isima/etudecaswebmobile/UserTest.java
+++ Backend/src/test/java/fr/isima/etudecaswebmobile/UserTest.java
@@ -1,4 +1,96 @@
 package fr.isima.etudecaswebmobile;
 
+
+import fr.isima.etudecaswebmobile.models.User;
+import fr.isima.etudecaswebmobile.repositories.UserRepository;
+import fr.isima.etudecaswebmobile.services.impl.UserImpl;
+import lombok.val;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+
+@RunWith(MockitoJUnitRunner.class)
 public class UserTest {
+
+    @InjectMocks
+    UserImpl userService;
+
+    @Mock
+    UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void getAllUsers()
+    {
+        List<User> list = new ArrayList<User>();
+        User userOne = new User();
+        userOne.setId_user((long)1);
+        userOne.setFirstName("mohamed");
+        userOne.setLastName("lidouh");
+        User userTwo = new User();
+        userTwo.setId_user((long)2);
+        userTwo.setFirstName("ilyass");
+        userTwo.setLastName("aitelkouch");
+        list.add(userOne);
+        list.add(userTwo);
+
+        when(userRepository.findAll()).thenReturn(list);
+
+        //test
+        List<User> userList = userService.getAllUsers();
+
+        assertEquals(2, userList.size());
+        verify(userRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void getUserByIdTest()
+    {
+        when(userRepository.findById((long)1)).thenReturn(Optional.of(new User((long) 1, "mohamed", "lidouh")));
+
+        Optional<User> user = userService.getUserById((long)1);
+
+        assertEquals("mohamed", user.get().getFirstName());
+        assertEquals("lidouh", user.get().getLastName());
+    }
+
+    @Test
+    public void createUserTest()
+    {
+
+
+        User user = new User();
+        user.setFirstName("redouane");
+        user.setUsername("redouane");
+        userService.addUser(user);
+
+        verify(userRepository, times(1)).save(user);
+    }
 }
+
+
+
+
+
