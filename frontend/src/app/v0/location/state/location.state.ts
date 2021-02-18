c1d8c2ddfcbaf7fe762153fd7c9ceb3bc2046de3
red-ait <redouanmkdem@gmail.com>
2021-02-18 20:54:45 +0100

    state

--- frontend/src/app/v0/location/state/location.state.ts
+++ frontend/src/app/v0/location/state/location.state.ts
@@ -4,7 +4,7 @@ import {Injectable} from '@angular/core';
 import {ITag} from '../../@entities/ITag';
 import * as locationAction from './location.action';
 import {LocationApi} from '../service/location.api';
-import {GetUserMapPointFail, GetUserMapPointSuccess} from "./location.action";
+import {GetUserMapPointFail, GetUserMapPointSuccess} from './location.action';
 
 export class LocationStateModel {
   mapPoints: Array<IMapPoint>;
@@ -27,7 +27,7 @@ export class LocationState {
   /************       App Selectors           ********************/
 
   @Selector()
-  static getMapPoint(state: LocationStateModel) {
+  static getMapPoints(state: LocationStateModel) {
     return state.mapPoints;
   }
 
