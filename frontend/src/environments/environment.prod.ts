fc06a24656e098f6a160240cd8e911405e45a98c
SIMOACR <medachkour@gmail.com>
2021-02-18 20:56:55 +0100

    backendApiUrl

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  backendApiUrl: '',
   apiUrl: '/api',
   resourceUri: {
     user: '/user'
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,6 +4,7 @@
 
 export const environment = {
   production: false,
+  backendApiUrl: '',
   apiUrl: '/api',
   resourceUri: {
     user: '/user'
