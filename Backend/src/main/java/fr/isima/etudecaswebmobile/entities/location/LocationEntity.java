6596893731344232b107edfc8c2b1c1ed0362859
SIMOACR <medachkour@gmail.com>
2021-03-19 11:52:55 +0100

    create locations with a list of tags #30

--- Backend/src/main/java/fr/isima/etudecaswebmobile/entities/location/LocationEntity.java
+++ Backend/src/main/java/fr/isima/etudecaswebmobile/entities/location/LocationEntity.java
@@ -25,11 +25,12 @@ public class LocationEntity {
     private double latitude;
     private double longitude;
 
-    @ManyToMany(cascade = {CascadeType.ALL}, fetch = FetchType.LAZY)
+    @ManyToMany(cascade = {CascadeType.ALL})
     @JoinTable(
             name = "locations_tags",
             joinColumns = @JoinColumn(name = "location_id"),
-            inverseJoinColumns = @JoinColumn(name = "tag_id"))
+            inverseJoinColumns = @JoinColumn(name = "tag_id")
+    )
     private List<TagEntity> tagEntities;
 
     public LocationEntity(Long id_location, String label, double latitude, double longitude)
