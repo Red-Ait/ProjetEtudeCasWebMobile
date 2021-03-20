e6272aa3251d5436d58f8852d76f15f9aca61dd2
Jamila <Jamila.akharaz@gmail.com>
2021-03-20 20:06:16 +0100

    Add Swagger for API documentation #15

--- Backend/src/main/java/fr/isima/etudecaswebmobile/config/WebSecurityConfig.java
+++ Backend/src/main/java/fr/isima/etudecaswebmobile/config/WebSecurityConfig.java
@@ -53,7 +53,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		// We don't need CSRF for this example
 		httpSecurity.csrf().disable()
 				// dont authenticate this particular request
-				.authorizeRequests().antMatchers("/authenticate", "/register").permitAll().
+				.authorizeRequests().antMatchers("/authenticate", "/register","/v2/api-docs", "/configuration/ui",
+				"/swagger-resources",
+				"/configuration/security",
+				"/swagger-ui.html",
+				"/swagger-ui/",
+				"/swagger-resources/configuration/ui",
+				"/webjars/**","/swagger.json",
+				"/swagger-resources/configuration/security").permitAll().
 				// all other requests need to be authenticated
 						anyRequest().authenticated().and().
 				// make sure we use stateless session; session won't be used to
