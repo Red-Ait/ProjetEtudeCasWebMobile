f9eeac2542bc56ef632669fe6fddb994835b11e9
red-ait <redouanmkdem@gmail.com>
2021-03-30 10:36:17 +0200

    resolved

--- frontend/src/app/v0/location/service/tag.service.ts
+++ frontend/src/app/v0/location/service/tag.service.ts
@@ -2,63 +2,50 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {environment} from '../../../../environments/environment';
-import {ITag} from "../../@entities/ITag";
+import {ITag} from '../../@entities/ITag';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TagService {
 
-  tagUri = '';
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) { }
+  Uri = environment.backendApiUrl + environment.apiUrl;
+  tagUri = environment.backendApiUrl + environment.apiUrl + environment.resourceUri.tag;
+  locationUri = environment.backendApiUrl + environment.apiUrl + environment.resourceUri.location;
 
-  private initUri() {
-    const uri = localStorage.getItem('uri');
-    this.Uri = uri + environment.apiUrl;
-    this.tagUri = uri + environment.apiUrl + environment.resourceUri.tag;
-    this.locationUri = uri + environment.apiUrl + environment.resourceUri.location;
-  }
+
+
   addTag(tag){
-    this.initUri();
-    return this.http.post(this.tagUri, tag).pipe(map(resp => resp));
+    return this.http.post(this.tagUri, tag);
   }
 
   getAllTag(): Observable<any> {
-    this.initUri();
     return this.http.get(this.tagUri);
   }
 
   deleteTag(idTag: number){
-    this.initUri();
     return this.http.delete(this.tagUri + '/' +  idTag);
   }
 
   updateTag(id, tag){
-    this.initUri();
-    return this.http.delete(this.tagUri + idTag).pipe(map(resp => resp));
-  }
+    return this.http.put(this.tagUri + '/' + id, tag);
 
-  updateTag(tag){
-    this.initUri();
-    return this.http.put(this.tagUri, tag).pipe(map(resp => resp));
   }
 
   getTagBylabel(label){
-    this.initUri();
-    return this.http.get(this.tagUri, label).pipe(map(resp => resp));
+    return this.http.get(this.tagUri, label);
+
   }
 
   shareLocationsWithAnotherUserByTagTitles(otherUsername: string, tagTitles: string[]){
-  // @ts-ignore
-        this.initUri();
-        return  this.http.put(this.locationUri + '/other_user' + '/' + otherUsername + '/tags/' + tagTitles);
-}
+    // @ts-ignore
+    return  this.http.put(this.locationUri + '/other_user' + '/' + otherUsername + '/tags/' + tagTitles);
+  }
 
   getUserNames(){
-        this.initUri();
-        return this.http.get(this.Uri + '/userNames' );
+    return this.http.get(this.Uri + '/userNames' );
   }
 
 }
